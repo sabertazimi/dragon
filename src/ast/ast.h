853b27111431f9f45bddc5ad1edba0f42d54b9ef
sabertazimi <sabertazimi@gmail.com>
2016-11-01 07:56:18 +0800

    refactor(dragon-ast): remove dive_into expression node(useless node)
    issue sabertazimi/awesome-notes#25

--- src/ast/ast.h
+++ src/ast/ast.h
@@ -38,13 +38,35 @@ typedef enum const_kind {
 } const_kind_t;
 
 /*
- * @implements: const_t
+ * @interface: const_t
  */
 typedef struct const_t {
     const_kind_t kind;
-    int value;
-    string text;
 } *const_t;
+
+/*
+ * @implements: const_int_t/const_bool_t
+ */
+typedef struct const_num_t {
+    const_kind_t kind;
+    int value;
+} *const_num_t;
+
+/*
+ * @implements: const_string_t
+ */
+typedef struct const_string_t {
+    const_kind_t kind;
+    string text;
+} *const_string_t;
+
+/*
+ * @implements: const_nil_t
+ */
+typedef struct const_nil_t {
+    const_kind_t kind;
+    string text;
+} *const_nil_t;
 /********** end of const_t **********/
 
 /********** start of func_def_t **********/
@@ -166,20 +188,12 @@ typedef struct expr_prim_nested_t {
 } *expr_prim_nested_t;
 
 /*
- * @implements: expr_prim_readint_t
+ * @implements: expr_prim_readint_t/expr_prim_readline_t
  */
-typedef struct expr_prim_readint_t {
+typedef struct expr_prim_read_t {
     expr_kind_t kind;
     expr_prim_kind_t sub_kind;
-} *expr_prim_readint_t;
-
-/*
- * @implements: expr_prim_readline_t
- */
-typedef struct expr_prim_readline_t {
-    expr_kind_t kind;
-    expr_prim_kind_t sub_kind;
-} *expr_prim_readline_t;
+} *expr_prim_read_t;
 
 /*
  * @implements: expr_prim_newclass_t
@@ -205,7 +219,6 @@ typedef struct expr_prim_newarray_t {
  * @brief: kind of expr_left_t
  */
 typedef enum expr_left_kind {
-    EXPR_LEFT_PRIM = 0,
     EXPR_LEFT_INDEX = 131,
     EXPR_LEFT_CLASS_FIELD,
     EXPR_LEFT_CLASS_CALL,
@@ -220,15 +233,6 @@ typedef struct expr_left {
     expr_kind_t kind;
     expr_left_kind_t sub_kind;
 } *expr_left_t;
-
-/*
- * @implements: expr_left_prim_t
- */
-typedef struct expr_left_prim {
-    expr_kind_t kind;
-    expr_left_kind_t sub_kind;
-    expr_prim_t body;
-} *expr_left_prim_t;
 
 /*
  * @implements: expr_left_index_t
@@ -285,7 +289,6 @@ typedef struct expr_left_anony_call {
  * @brief: kind of expr_unary_t
  */
 typedef enum expr_unary_kind {
-    EXPR_UNARY_LEFT = 0,
     EXPR_UNARY_THIS = 121,
     EXPR_UNARY_PLUS,
     EXPR_UNARY_MINUS,
@@ -293,109 +296,55 @@ typedef enum expr_unary_kind {
 } expr_unary_kind_t;
 
 /*
- * @interface: expr_unary_t
+ * @implements: expr_unary_this_t/expr_unary_plus_t/expr_unary_minus_t/expr_unary_not_t
  */
 typedef struct expr_unary {
     expr_kind_t kind;
     expr_unary_kind_t sub_kind;
-} *expr_unary_t;
-
-/*
- * @implements: expr_unary_left_t
- */
-typedef struct expr_unary_left {
-    expr_kind_t kind;
-    expr_unary_kind_t sub_kind;
-    expr_left_t body;
-} *expr_unary_left_t;
-
-/*
- * @implements: expr_unary_this_t/expr_unary_plus_t/expr_unary_minus_t/expr_unary_not_t
- */
-typedef struct expr_unary_normal {
-    expr_kind_t kind;
-    expr_unary_kind_t sub_kind;
     expr_unary_t body;
-} *expr_unary_normal_t;
+} *expr_unary_t;
 
 /*
  * @brief: kind of expr_mul_t
  */
 typedef enum expr_mul_kind {
-    EXPR_MUL_UNARY = 0,
     EXPR_MUL_MUL = 111,
     EXPR_MUL_DIV,
     EXPR_MUL_MOD
 } expr_mul_kind_t;
 
 /*
- * @interface: expr_mul_t
+ * @implements: expr_mul_mul_t/expr_mul_sub_t/expr_mul_mod_t
  */
 typedef struct expr_mul {
     expr_kind_t kind;
     expr_mul_kind_t sub_kind;
-} *expr_mul_t;
-
-/*
- * @implements: expr_mul_unary_t
- */
-typedef struct expr_mul_unary {
-    expr_kind_t kind;
-    expr_mul_kind_t sub_kind;
-    expr_unary_t body;
-} *expr_mul_unary_t;
-
-/*
- * @implements: expr_mul_mul_t/expr_mul_sub_t/expr_mul_mod_t
- */
-typedef struct expr_mul_normal {
-    expr_kind_t kind;
-    expr_mul_kind_t sub_kind;
     expr_mul_t left;
     expr_unary_t right;
-} *expr_mul_normal_t;
+} *expr_mul_t;
 
 /*
  * @brief: kind of expr_add_t
  */
 typedef enum expr_add_kind {
-    EXPR_ADD_MUL = 0,
     EXPR_ADD_ADD = 101,
     EXPR_ADD_SUB
 } expr_add_kind_t;
 
 /*
- * @interface: expr_add_t
+ * @implements: expr_add_add_t/expr_add_sub_t
  */
 typedef struct expr_add {
     expr_kind_t kind;
     expr_add_kind_t sub_kind;
-} *expr_add_t;
-
-/*
- * @implements: expr_add_mul_t
- */
-typedef struct expr_add_mul {
-    expr_kind_t kind;
-    expr_add_kind_t sub_kind;
-    expr_mul_t body;
-} *expr_add_mul_t;
-
-/*
- * @implements: expr_add_add_t/expr_add_sub_t
- */
-typedef struct expr_add_normal {
-    expr_kind_t kind;
-    expr_add_kind_t sub_kind;
     expr_add_t left;
     expr_mul_t right;
-} *expr_add_normal_t;
+} *expr_add_t;
 
 /*
  * @brief: kind of expr_cmp_t
  */
 typedef enum expr_cmp_kind {
-    EXPR_CMP_ADD = 0,
     EXPR_CMP_L = 91,
     EXPR_CMP_G,
     EXPR_CMP_LE,
@@ -403,172 +352,83 @@ typedef enum expr_cmp_kind {
 } expr_cmp_kind_t;
 
 /*
- * @interface: expr_cmp_t
+ * @implements: expr_cmp_l_t/expr_cmp_g_t/expr_cmp_le_t/expr_cmp_ge_t
  */
 typedef struct expr_cmp {
     expr_kind_t kind;
     expr_cmp_kind_t sub_kind;
-} *expr_cmp_t;
-
-/*
- * @implements: expr_cmp_add_t
- */
-typedef struct expr_cmp_add {
-    expr_kind_t kind;
-    expr_cmp_kind_t sub_kind;
-    expr_add_t body;
-} *expr_cmp_add_t;
-
-/*
- * @implements: expr_cmp_l_t/expr_cmp_g_t/expr_cmp_le_t/expr_cmp_ge_t
- */
-typedef struct expr_cmp_normal {
-    expr_kind_t kind;
-    expr_cmp_kind_t sub_kind;
     expr_cmp_t left;
     expr_add_t right;
-} *expr_cmp_normal_t;
+} *expr_cmp_t;
 
 /*
  * @brief: kind of expr_eq_t
  */
 typedef enum expr_eq_kind {
-    EXPR_EQ_CMP = 0,
     EXPR_EQ_EQ = 81,
     EXPR_EQ_NE
 } expr_eq_kind_t;
 
 /*
- * @interface: expr_eq_t
+ * @implements: expr_eq_eq_t/expr_eq_ne_t
  */
 typedef struct expr_eq {
     expr_kind_t kind;
     expr_eq_kind_t sub_kind;
-} *expr_eq_t;
-
-/*
- * @implements: expr_eq_cmp_t
- */
-typedef struct expr_eq_cmp {
-    expr_kind_t kind;
-    expr_eq_kind_t sub_kind;
-    expr_cmp_t body;
-} *expr_eq_cmp_t;
-
-/*
- * @implements: expr_eq_eq_t/expr_eq_ne_t
- */
-typedef struct expr_eq_normal {
-    expr_kind_t kind;
-    expr_eq_kind_t sub_kind;
     expr_eq_t left;
     expr_cmp_t right;
-} *expr_eq_normal_t;
+} *expr_eq_t;
 
 /*
  * @brief: kind of expr_and_t
  */
 typedef enum expr_and_kind {
-    EXPR_AND_EQ = 0,
     EXPR_AND_NORMAL = 71
 } expr_and_kind_t;
 
 /*
- * @interface: expr_and_t
+ * @implements: expr_and_t
  */
 typedef struct expr_and {
     expr_kind_t kind;
     expr_and_kind_t sub_kind;
-} *expr_and_t;
-
-/*
- * @implements: expr_and_eq_t
- */
-typedef struct expr_and_eq {
-    expr_kind_t kind;
-    expr_and_kind_t sub_kind;
-    expr_eq_t body;
-} *expr_and_eq_t;
-
-/*
- * @implements: expr_and_normal_t
- */
-typedef struct expr_and_normal {
-    expr_kind_t kind;
-    expr_and_kind_t sub_kind;
     expr_and_t left;
     expr_eq_t right;
-} *expr_and_normal_t;
+} *expr_and_t;
 
 /*
  * @brief: kind of expr_or_t
  */
 typedef enum expr_or_kind {
-    EXPR_OR_AND = 0,
     EXPR_OR_NORMAL = 61
 } expr_or_kind_t;
 
 /*
- * @interface: expr_or_t
+ * @implements: expr_or_t
  */
 typedef struct expr_or {
     expr_kind_t kind;
     expr_or_kind_t sub_kind;
-} *expr_or_t;
-
-/*
- * @implements: expr_or_and_t
- */
-typedef struct expr_or_and {
-    expr_kind_t kind;
-    expr_or_kind_t sub_kind;
-    expr_and_t body;
-} *expr_or_and_t;
-
-/*
- * @implements: expr_or_normal_t
- */
-typedef struct expr_or_normal {
-    expr_kind_t kind;
-    expr_or_kind_t sub_kind;
     expr_or_t left;
     expr_and_t right;
-} *expr_or_normal_t;
+} *expr_or_t;
 
 /*
  * @brief: kind of expr_assign_t
  */
 typedef enum expr_assign_kind {
-    EXPR_ASSIGN_OR = 0,
     EXPR_ASSIGN_NORMAL = 51
 } expr_assign_kind_t;
 
 /*
- * @interface: expr_assign_t
+ * @implements: expr_assign_t
  */
-struct expr_assign_t {
-    expr_kind_t kind;
-    expr_assign_kind_t sub_kind;
-};
-
-/*
- * @implements: expr_assign_or_t
- */
-typedef struct expr_assign_or {
-    expr_kind_t kind;
-    expr_assign_kind_t sub_kind;
-    expr_or_t body;
-} *expr_assign_or_t;
-
-/*
- * @implements: expr_assign_normal_t
- */
-typedef struct expr_assign_normal {
+typedef struct expr_assign {
     expr_kind_t kind;
     expr_assign_kind_t sub_kind;
     expr_left_t left;
     expr_assign_t right;
-} *expr_assign_normal_t;
+} *expr_assign_t;
 
 /*
  * @interface: expr_bool_t
