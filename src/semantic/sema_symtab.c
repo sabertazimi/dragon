fcd80c1d985cde5d0d3741c26895a39a8158ca3c
sabertazimi <sabertazimi@gmail.com>
2016-11-05 20:26:12 +0800

    update(dragon-semantic): notes for how to implement scope and symbol table
    1. relationships between ast node, scope_t and symtab_t
    
    *   ast node(class_def and func_def) has pointer member pointing to a scope_t variable, to store their scope
    *   scope_t has a symtab_t member ,to store all symbol valid in this scope
    *   symtab_t has a stack_t/list_t (<stack_t/list_t <symbol_t>) member, to store all defs binding to symbols
    
    ```c
    typedef struct _symbol_ *symbol_t
    struct _symbol_ {
        const char* id;     // key
        def_t def;          // value
    };
    ```
    
    when travel ast tree first time, set up all scope_t in class_def_t or func_def_t:
    
    1. create empty scope
    2. bind a specific def_t to a symbol_t (set up id -> def_t mapping)
    3. add symbol_t to scope->symtab_t
    
    when travel ast tree second time, start to truly semantic analysis(e.g type check).
    
    @TODO:
    
    * change ast node <class_def_t> <func_def_t> , add scope_t member
    * change ast node <without scope_t>, add parent member
    * change parser.y and new ast node function <class_def_t, func_def_t>, bind all children of them to them(children->parent = this/them)
    * implement data structure symbol_t, symtab_t and scope_t and operations on them.
    
    issue sabertazimi/awesome-notes#25

--- src/semantic/sema_symtab.c
+++ src/semantic/sema_symtab.c
@@ -7,6 +7,30 @@
 
 #include "sema_symtab.h"
 
+/*
+ * @TODO: get inspiration from cs143
+ * relationships between ast node, scope_t and symtab_t
+ * ast node(class_def and func_def) has pointer member pointing to a scope_t variable, to store their scope
+ * scope_t has a symtab_t member ,to store all symbol valid in this scope
+ * symtab_t has a stack_t/list_t (<stack_t/list_t <symbol_t>) member, to store all defs binding to symbols
+ * typedef struct _symbol_ {
+ *  const char* id;     // key
+ *  def_t def;          // value
+ * } *symbol_t;
+ *
+ * when travel ast tree first time, set up all scope_t in class_def_t or func_def_t
+ * 1. create empty scope
+ * 2. bind a specific def_t to a symbol_t (set up id -> def_t mapping)
+ * 3. add symbol_t to scope->symtab_t
+ *
+ * when travel ast tree second time, start to truly semantic analysis(e.g type check)
+ *
+ * @TODO: change ast node <class_def_t> <func_def_t> , add scope_t member
+ * @TODO: change ast node <without scope_t>, add parent member
+ * @TODO: change parser.y and new ast node function <class_def_t, func_def_t>, bind all children of them to them(children->parent = this/them)
+ * @TODO: implement data structure symbol_t, symtab_t and scope_t and operations on them.
+ */
+
 static yyltype mock_loc = {233, 233, 233, 234};
 
 // scope_t
